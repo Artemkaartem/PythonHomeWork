9058469eb462439d038be56eb059694ebef6a107
artem <kuteinikovartemka@yandex.ru>
2023-05-30 16:17:39 +0300

    Homework10

---
+++ HomeWork10/Task44.py
@@ -0,0 +1,14 @@
+import pandas as bd
+import numpy as np
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = bd.DataFrame({'whoAmI':lst})
+data.head()
+
+data['human'] = np.where(data['whoAmI'] == 'human', '1', '0')
+data['robot'] = np.where(data['whoAmI'] == 'robot', '1', '0')
+
+print(data)
